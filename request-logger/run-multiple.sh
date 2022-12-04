d4fc73595b1564a9e76db2fa7c1370ea22123a69
Fiona W <99679008+F3F3exe@users.noreply.github.com>
2022-12-04 20:21:04 +0100

    Update run-multiple.sh

--- request-logger/run-multiple.sh
+++ request-logger/run-multiple.sh
@@ -6,24 +6,25 @@
 script_dir=$(dirname "$0")
 crawler="${script_dir}/run.js"
 google_chrome_binary="/opt/google/chrome/google-chrome"
-google_chrome_profile="PATH"
-metamask_path="PATH"
-
+google_chrome_profile="/home/fefe/.config/google-chrome/Default"
+metamask_path="~/.config/google-chrome/Default/Extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/10.22.2_0"
+echo 'hello'
 if [ $# != 1 ]
 then
     >&2 echo "Usage: $0 FILE"
     exit 1
 fi
 file_name="$1"
-
+echo $file_name
 while read url; do
   echo "Crawling ${url}."
   domain=$(echo "$url" | awk -F/ '{print $3}')
   "$crawler" \
     --interactive \
     --binary "$google_chrome_binary" \
-    --profile "$google_chrome_profile" \
+    --debug verbose \
     --metamask "$metamask_path" \
     --ancestors \
-    --url "$url" > "${domain}.json"
+    --url "$url" > "${domain}.json" 
 done <"$file_name"
+   # --profile "$google_chrome_profile" \
